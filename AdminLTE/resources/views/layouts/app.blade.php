9e5f2c7a2eb597e1153307205e3a324246550996
Eduiin Maay <61023101+EduinMay12@users.noreply.github.com>
2021-06-16 13:53:10 -0500

    Front-end perfiles y home

--- AdminLTE/resources/views/layouts/app.blade.php
+++ AdminLTE/resources/views/layouts/app.blade.php
@@ -1,79 +1,122 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8" />
+    <title>{{ config('app.name', 'Gestion de Cambio | Inicio') }}</title>
+    <!-- Bootstrap Css -->
+    <link href="{{ url('css/bootstrap.rtl.css') }}" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="{{ url('css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="{{ url('css/app.rtl.css') }}" rel="stylesheet" type="text/css" />
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="http://minible-h-rtl.laravel.themesbrand.com/assets/libs/select2/select2.min.css" rel="stylesheet" type="text/css" />
+    <link href="http://minible-h-rtl.laravel.themesbrand.com/assets/libs/spectrum-colorpicker/spectrum-colorpicker.min.css" rel="stylesheet">
+    <link href="http://minible-h-rtl.laravel.themesbrand.com/assets/libs/bootstrap-datepicker/bootstrap-datepicker.min.css" rel="stylesheet">
+    <link href="http://minible-h-rtl.laravel.themesbrand.com/assets/libs/bootstrap-touchspin/bootstrap-touchspin.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="http://minible-h-rtl.laravel.themesbrand.com/assets/libs/datepicker/datepicker.min.css">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+<body data-layout="horizontal" data-topbar="colored">
+    <!-- Navbar -->
+        <header id="page-topbar">
+            <div class="navbar-header">
+                <div class="dropdown d-inline-block">
+                    <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
+                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="uil-angle-down d-none d-xl-inline-block font-size-15"></i>
+                        <span class="d-none d-xl-inline-block ms-1 fw-medium font-size-15"> {{ Auth::user()->name }}</span>
+                        <img class="rounded-circle header-profile-user" src="../uploads/avatars/{{ auth()->user()->avatar }}"
+                            alt="Header Avatar">
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-end">
+                        <!-- item-->
+                        <a class="dropdown-item" href="/home"><span class="align-middle">{{ __('Inicio') }}</span> <i class="uil-home-alt font-size-18 align-middle text-muted me-1"></i></a>
 
-                    </ul>
+                        <a class="dropdown-item" href="/perfil/edit"><span class="align-middle">{{ __('Perfil') }}</span> <i class="uil uil-user-circle font-size-18 align-middle text-muted me-1"></i></a>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+                        <a class="dropdown-item d-block" href="/administrador"><span class="align-middle">{{ __('Administrador') }}</span> <i class="uil uil-cog font-size-18 align-middle me-1 text-muted"></i></a>
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                      document.getElementById('logout-form').submit();">
+                         {{ __('Salir') }}<i class="uil uil-sign-out-alt font-size-18 align-middle me-1 text-muted"></i>
+                        </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                         @csrf
+                     </form>
+                    </div>
+                </div>
+                <div class="d-flex">
+                    <div class="d-flex">
+                        <!-- LOGO -->
+                        <div class="navbar-brand-box">
+                            <a href="/home" class="logo logo-dark">
+                                <span class="logo-sm">
+                                    <img src="vendor/adminlte/dist/img/edumatics.png" alt="" height="32">
+                                </span>
+                                <span class="logo-lg">
+                                    <img src="vendor/adminlte/dist/img/edumatics.png" alt="" height="30">
+                                </span>
+                            </a>
+
+                            <a href="/home" class="logo logo-light">
+                                <span class="logo-sm">
+                                    <img src="vendor/adminlte/dist/img/edumatics.png" alt="" height="32">
+                                </span>
+                                <span class="logo-lg">
+                                    <img src="vendor/adminlte/dist/img/edumatics.png" alt="" height="30">
+                                </span>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </nav>
+            @yield('header-gestion')
+        </header>
+    <!-- ============================================================== -->
+        <!-- Contenido del sistema -->
+    <!-- ============================================================== -->
+    <div class="main-content">
+        <div class="page-content">
 
-        <main class="py-4">
             @yield('content')
-        </main>
+
+            <footer class="footer">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <script>document.write(new Date().getFullYear())</script> © EDUMATICS
+                        </div>
+
+                    </div>
+                </div>
+            </footer>
+        </div>
     </div>
+
+    <!-- JAVASCRIPT -->
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/metismenu.min.js') }}"></script>
+    <script src="{{ asset('js/simplebar.min.js') }}"></script>
+    <script src="{{ asset('js/node-waves.min.js') }}"></script>
+    <script src="{{ asset('js/waypoints.min.js') }}"></script>
+    <script src="{{ asset('js/jquery-counterup.min.js') }}"></script>
+    <script src="{{ asset('js/app.min.js') }}"></script>
+
+    <script src="http://minible-h-rtl.laravel.themesbrand.com/assets/libs/select2/select2.min.js"></script>
+    <script src="http://minible-h-rtl.laravel.themesbrand.com/assets/libs/spectrum-colorpicker/spectrum-colorpicker.min.js"></script>
+    <script src="http://minible-h-rtl.laravel.themesbrand.com/assets/libs/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+    <script src="http://minible-h-rtl.laravel.themesbrand.com/assets/libs/bootstrap-touchspin/bootstrap-touchspin.min.js"></script>
+    <script src="http://minible-h-rtl.laravel.themesbrand.com/assets/libs/bootstrap-maxlength/bootstrap-maxlength.min.js"></script>
+    <script src="http://minible-h-rtl.laravel.themesbrand.com/assets/libs/datepicker/datepicker.min.js"></script>
+    <script src="http://minible-h-rtl.laravel.themesbrand.com/assets/js/pages/form-advanced.init.js"></script>
 </body>
+
 </html>
+
