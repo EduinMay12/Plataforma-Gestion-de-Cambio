071b334e1cd919daa289b7d2d099a9743ec3e73e
Emiliano Cocom <joseemilianococom@gmail.com>
2021-06-21 08:01:07 -0500

    gitignore funcionando

---
+++ AdminLTE/app/Models/ModuloCapacitaciones/Instructore.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\ModuloCapacitaciones;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Instructore extends Model
+{
+    use HasFactory;
+}
