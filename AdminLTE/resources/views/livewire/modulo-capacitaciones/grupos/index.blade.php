755b60899111a8deb36d6568a10b9fecdde5e525
Emiliano Cocom <joseemilianococom@gmail.com>
2021-07-10 13:32:59 -0500

    Archivos y configuración del crud de grupos terminados

--- AdminLTE/resources/views/livewire/modulo-capacitaciones/grupos/index.blade.php
+++ AdminLTE/resources/views/livewire/modulo-capacitaciones/grupos/index.blade.php
@@ -3,23 +3,9 @@
         <div class="card">
             <div class="card-body">
 
-                @include("livewire.modulo-capacitaciones.grupos.$view")
+                @include('livewire.modulo-capacitaciones.grupos.table')
 
             </div>
         </div>
     </div>
-
-    <!-- Scripts ---->
-    @livewireScripts
-
-    <script src="https://cdn.ckeditor.com/ckeditor5/29.0.0/classic/ckeditor.js"></script>
-    
-    <script>
-        ClassicEditor
-            .create( document.querySelector( '#editor' ) )
-            .catch( error => {
-                console.error( error );
-            } );
-    </script>
-
 </div>
