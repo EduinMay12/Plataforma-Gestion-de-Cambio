6450fc4ee52b564d2336b530ae450f7595c0c57c
Emiliano Cocom <joseemilianococom@gmail.com>
2021-08-04 18:41:53 -0500

    empece el crud de matriculaciones

--- AdminLTE/resources/views/livewire/modulo-capacitaciones/matriculaciones/form.blade.php
+++ AdminLTE/resources/views/livewire/modulo-capacitaciones/matriculaciones/form.blade.php
@@ -20,8 +20,8 @@
 <div class="row mt-2">
 
     <div class="col-4">
-        <label for="">Seleccionar sucursal</label>
-        <select class="form-control">
+        <label for="">Seleccionar sucursal {{$sucursal_id}}</label>
+        <select wire:model="sucursal_id" class="form-control">
             <option value="">Seleccione...</option>
             @foreach($sucursales as $sucursal)
                 <option value="{{$sucursal->id}}">{{$sucursal->sucursal}}</option>
@@ -30,13 +30,22 @@
     </div>
 
     <div class="col-4">
-        <label for="">Seleccionar perfil</label>
-        <select class="form-control">
+        <label for="">Seleccionar perfil {{$role_id}}</label>
+        <select wire:model="role_id" class="form-control">
             <option value="">Seleccione...</option>
             @foreach($roles as $role)
                 <option value="{{$role->id}}">{{$role->name}}</option>
             @endforeach
         </select>
     </div>
+
+    @if($participantes->count())
+        @foreach($participantes as $participante)
+            <span>{{$participante->name}}</span>
+        @endforeach
+    @else
+        <span>No existen registros</span>
+    @endif
+
 
 </div>
