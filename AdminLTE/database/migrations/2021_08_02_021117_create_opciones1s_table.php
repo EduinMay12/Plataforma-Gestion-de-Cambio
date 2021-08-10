a89765693242820265b7d075fd1027a12ab8e17a
Emiliano Cocom <joseemilianococom@gmail.com>
2021-08-09 19:34:21 -0500

    Merge remote-tracking branch 'remotes/origin/Eduin_May' into Emiliano_Cocom
    # Conflicts:
    #	AdminLTE/app/Http/Controllers/ModuloDiagnosticos/Respuestas2Controller.php
    #	AdminLTE/app/Http/Livewire/ModuloCapacitaciones/Matriculaciones/Index.php
    #	AdminLTE/app/Http/Livewire/ModuloCapacitaciones/Preguntas/Index.php
    #	AdminLTE/app/Http/Livewire/ModuloCapacitaciones/Recursos/Index.php
    #	AdminLTE/app/Models/ModuloDiagnosticos/Cuestionario1.php
    #	AdminLTE/config/adminlte.php
    #	AdminLTE/database/migrations/2021_06_24_173532_create_opciones_table.php
    #	AdminLTE/database/migrations/2021_08_02_021117_create_opciones1s_table.php
    #	AdminLTE/database/migrations/2021_08_04_181027_create_opciones2s_table.php
    #	AdminLTE/database/seeders/DatabaseSeeder.php
    #	AdminLTE/database/seeders/PermissionTableSeeder.php
    #	AdminLTE/resources/views/livewire/modulo-administrador/user/table.blade.php
    #	AdminLTE/routes/web.php

--- AdminLTE/database/migrations/2021_08_02_021117_create_opciones1s_table.php
+++ AdminLTE/database/migrations/2021_08_02_021117_create_opciones1s_table.php
@@ -4,7 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+<<<<<<< HEAD:AdminLTE/database/migrations/2021_08_02_021117_create_opciones1s_table.php
 class CreateOpciones1sTable extends Migration
+=======
+class CreateOpciones2sTable extends Migration
+>>>>>>> remotes/origin/Eduin_May:AdminLTE/database/migrations/2021_08_04_181027_create_opciones2s_table.php
 {
     /**
      * Run the migrations.
@@ -13,7 +17,11 @@ class CreateOpciones1sTable extends Migration
      */
     public function up()
     {
+<<<<<<< HEAD:AdminLTE/database/migrations/2021_08_02_021117_create_opciones1s_table.php
         Schema::create('opciones1s', function (Blueprint $table) {
+=======
+        Schema::create('opciones2s', function (Blueprint $table) {
+>>>>>>> remotes/origin/Eduin_May:AdminLTE/database/migrations/2021_08_04_181027_create_opciones2s_table.php
             $table->id();
             $table->string('opcion');
             $table->integer('valor');
@@ -23,7 +31,11 @@ class CreateOpciones1sTable extends Migration
 
             $table->foreign('pregunta_id')
                 ->references('id')
+<<<<<<< HEAD:AdminLTE/database/migrations/2021_08_02_021117_create_opciones1s_table.php
                 ->on('preguntas2s')
+=======
+                ->on('preguntas3s')
+>>>>>>> remotes/origin/Eduin_May:AdminLTE/database/migrations/2021_08_04_181027_create_opciones2s_table.php
                 ->onDelete('cascade');
                 
             $table->timestamps();
@@ -37,6 +49,10 @@ class CreateOpciones1sTable extends Migration
      */
     public function down()
     {
+<<<<<<< HEAD:AdminLTE/database/migrations/2021_08_02_021117_create_opciones1s_table.php
         Schema::dropIfExists('opciones1s');
+=======
+        Schema::dropIfExists('opciones2s');
+>>>>>>> remotes/origin/Eduin_May:AdminLTE/database/migrations/2021_08_04_181027_create_opciones2s_table.php
     }
 }
